8843313d786c573ac40680877bd3bbde16957153
Farzana761 <farzanaaktar761@gmail.com>
2026-03-26 16:45:28 +0600

    initial commit

---
+++ dummy.py
@@ -0,0 +1,16 @@
+import pytest
+import random
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture()
+def driver():
+    service = Service(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=options)
+
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
